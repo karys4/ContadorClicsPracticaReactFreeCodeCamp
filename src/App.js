7f02928109121717b2d9304d2660164e1b5d6f56
karyrodriguez <kary.github@gmail.com>
2022-04-20 13:22:25 -0500

    Ejemplo de aplicacion React: Contador de clics con useState que es un Hook

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import "./App.css";
+import Boton from "./componentes/Boton";
+import logo from "./imagenes/freecodecamp-logo.png";
+import Contador from "./componentes/Contador";
+import { useState } from "react"; //Hook
+
+function App() {
+  //Manejar por medio de estados
+  const [numClics, setNumClics] = useState(0);
+
+  const manejarClic = () => {
+    setNumClics(numClics + 1);
+  };
+
+  const reiniciarContador = () => {
+    setNumClics(0);
+  };
+
+  return (
+    <div className="App">
+      <div className="freecodecamp-logo-contenedor">
+        <img
+          className="freecodecamp-logo"
+          src={logo}
+          alt="Logo de freecodecamp "
+        />
+      </div>
+      <div className="contenedor-principal">
+        <Contador numClics={numClics} />
+        <Boton texto="Clic" esBotonDeClic={true} manejarClic={manejarClic} />
+        <Boton
+          texto="Reiniciar"
+          esBotonDeClic={false}
+          manejarClic={reiniciarContador}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
